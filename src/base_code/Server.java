15f03aacc3a22786d82b2427dc9092ba196e35b7
Vitaliy <mr.prikota@gmail.com>
2019-08-20 17:09:57 +0300

    Main server and file loader servers are running in 2 different threads.

--- src/base_code/Server.java
+++ src/base_code/Server.java
@@ -1,37 +1,91 @@
 package base_code;
 
+
+import multithread_classes.FileLoaderThread;
 import multithread_classes.ThreadServer;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class Server {
-    private LinkedList<ThreadServer> serverThreads;
 
-    public Server(int port) throws IOException {
-        serverThreads = new LinkedList<>();
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Server started");
-        try {
-            while (true) {
-                Socket socket = serverSocket.accept();
+
+    public Server(int serverPort, int fileLoaderPort) throws IOException {
+        new ServerListener(serverPort);
+        new FileLoaderListener(fileLoaderPort);
+        System.out.println("Servers started");
+    }
+
+    public class ServerListener extends Thread {
+        private LinkedList<ThreadServer> serverThreads;
+        private ServerSocket serverSocket;
+
+        public ServerListener(int port) throws IOException {
+            serverThreads = new LinkedList<>();
+            serverSocket = new ServerSocket(port);
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    try {
+                        serverThreads.add(new ThreadServer(socket));
+                    } catch (IOException e) {
+                        socket.close();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
                 try {
-                    serverThreads.add(new ThreadServer(socket));
+                    serverSocket.close();
                 } catch (IOException e) {
-                    socket.close();
+                    e.printStackTrace();
                 }
             }
-        } finally {
-            serverSocket.close();
+        }
+    }
+
+    public class FileLoaderListener extends Thread {
+        private LinkedList<FileLoaderThread> fileLoaderThreads;
+        private ServerSocket serverSocket;
+
+        public FileLoaderListener(int port) throws IOException {
+            fileLoaderThreads = new LinkedList<>();
+            serverSocket = new ServerSocket(port);
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    try {
+                        fileLoaderThreads.add(new FileLoaderThread(socket));
+                    } catch (IOException e) {
+                        socket.close();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public static void main(String[] args) throws IOException {
-        Server server = new Server(45777);
+        Server server = new Server(45777, 45778);
     }
 }
---
+++ src/multithread_classes/FileLoaderThread.java
@@ -0,0 +1,10 @@
+package multithread_classes;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class FileLoaderThread extends Thread {
+    public FileLoaderThread(Socket socket) throws IOException {
+
+    }
+}
